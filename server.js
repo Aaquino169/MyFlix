3bd7f882313d1cb1e645de553273625399f6db7b
alexander_aquino <nanito169@gmail.com>
2020-10-22 14:09:15 -0400

    connected to mongoose

--- server.js
+++ server.js
@@ -2,7 +2,28 @@ const express = require('express')
 
 const app = express()
 
-const PORT = 3000
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+
+//env varables
+const PORT = process.env.PORT
+const mongodbURI = process.env.MONGODBURI
+
+//mongoose connection
+mongoose.connect(mongodbURI,{
+    useFindAndModify:false,
+    useNewUrlParser:true,
+    useUnifiedTopology:false
+},() => {
+    console.log('connected to mongo')
+})
+
+//homepage
+app.get('/',(req,res) => {
+    res.send('home page')
+})
+
 
 app.listen(PORT, () => {
     console.log('App is using port:', PORT)
