fad281fcf4e2010694485ee25c0a129d3c5d4ff4
alexander_aquino <nanito169@gmail.com>
2020-10-24 13:15:08 -0400

    Created user page&& linked to db

--- server.js
+++ server.js
@@ -1,24 +1,36 @@
+
 const express = require('express')
 
 const app = express()
 
 const mongoose = require('mongoose')
 
+const session = require('express-session')
+
 
 require('dotenv').config()
 
 //env varables
 const PORT = process.env.PORT
+
 const mongodbURI = process.env.MONGODBURI
 
 const db = mongoose.connection
 
 const bodyParser = require('body-parser')
+
 //Middleware
 app.use(express.urlencoded({extended:true}))
 
 app.use(express.static('public'))
 
+app.use(
+    session({
+      secret: process.env.SECRET, //a random string do not copy this value or your stuff will get hacked
+      resave: false, // default more info: https://www.npmjs.com/package/express-session#resave
+      saveUninitialized: false // default  more info: https://www.npmjs.com/package/express-session#resave
+    })
+  )
 //mongoose connection
 mongoose.connect(mongodbURI,{
     useFindAndModify:false,
@@ -37,6 +49,9 @@ app.use('/users', usersController)
 
 const sessionsController = require('./controllers/sessions')
 app.use('/sessions',sessionsController)
+
+const userController = require('./controllers/users')
+app.use('/users', userController)
 
 
 app.listen(PORT, () => {
